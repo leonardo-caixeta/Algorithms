630cab2fdab876bc9b54b699f04921a69730b49f
leonardo <leonardocaixeta07@gmail.com>
2023-11-29 16:34:13 -0300

    feat:  requisito 2

--- tests/encrypt/test_encrypt.py
+++ tests/encrypt/test_encrypt.py
@@ -1,5 +1,15 @@
-# from challenges.challenge_encrypt_message import encrypt_message
+from challenges.challenge_encrypt_message import encrypt_message
+import pytest
 
 
 def test_encrypt_message():
-    pass
+    assert encrypt_message("ola mundo", 3) == "alo_odnum "
+
+    assert encrypt_message("ola mundo", 4) == "odnum_ alo"
+
+    assert encrypt_message("ola mundo", 9) == "odnum alo"
+
+    with pytest.raises(
+        TypeError, match="tipo inválido para message"
+    ):
+        encrypt_message(10, 1)
