e567945375a1ae0040b2a528bea21e8ba37fd39c
leonardo <leonardocaixeta07@gmail.com>
2023-11-29 14:58:18 -0300

    feat: requisito 1

--- challenges/challenge_study_schedule.py
+++ challenges/challenge_study_schedule.py
@@ -1,3 +1,15 @@
 def study_schedule(permanence_period, target_time):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    students_per_target_time = 0
+    if target_time is None:
+        return None
+
+    for comein, comeout in permanence_period:
+        if (comein is None or not type(comein) is int) or (
+            comeout is None or not type(comeout) is int
+        ):
+            return None
+
+        if comeout >= target_time >= comein:
+            students_per_target_time += 1
+
+    return students_per_target_time
