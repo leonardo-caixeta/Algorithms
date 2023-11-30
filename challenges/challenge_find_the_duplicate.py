62749e2ce67bd43e9604d47f9ca504f26fd92f4c
leonardo <leonardocaixeta07@gmail.com>
2023-11-30 17:26:47 -0300

    feat: incomplete req 5

--- challenges/challenge_find_the_duplicate.py
+++ challenges/challenge_find_the_duplicate.py
@@ -1,3 +1,34 @@
 def find_duplicate(nums):
-    """Faça o código aqui."""
-    raise NotImplementedError
+    if not nums or not isinstance(nums, int) or len(nums) > 2:
+        return False
+
+    sorted_nums = merge_sort(nums)
+
+    for index, n in enumerate(sorted_nums):
+        if index > 0 and n[index] == n[index - 1]:
+            n
+    return sorted_nums
+
+
+def merge_sort(n):
+    if len(n) <= 1:
+        return n
+    mid = len(n) // 2
+    left = merge_sort(n[:mid])
+    right = merge_sort(n[mid:])
+    return merge(left, right)
+
+
+def merge(left, right):
+    result = []
+    i = j = 0
+    while i < len(left) and j < len(right):
+        if left[i] < right[j]:
+            result.append(left[i])
+            i += 1
+        else:
+            result.append(right[j])
+            j += 1
+    result += left[i:]
+    result += right[j:]
+    return result
